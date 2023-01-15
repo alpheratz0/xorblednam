2843fef232534823700abd09bc5dab6851e7920b
alpheratz0 <alpheratz99@protonmail.com>
2023-01-14 22:30:14 -0300

    bump version to 0.1.5

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
-# Copyright (C) 2022 <alpheratz99@protonmail.com>
+# Copyright (C) 2022-2023 <alpheratz99@protonmail.com>
 # This program is free software.
 
-VERSION   = 0.1.4
+VERSION   = 0.1.5
 
 CC        = cc
 CFLAGS    = -std=c99 -pedantic -Wall -Wextra -Os -DVERSION=\"$(VERSION)\"
